25e9d2e91982dbb7f6d4c4868bf9c96f5dd3e865
edgar2420 <edgarrojas082@gmail.com>
2024-12-01 23:57:44 -0400

    actualizacion

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "ecommerce",
+    HOST: "bglsgnzrg1n8vgdzopps-mysql.services.clever-cloud.com",
+    USER: "usc0kmebmezqqj9z",
+    PASSWORD: "JUo8Rh9bBa910rzSpTrL",
+    DB: "bglsgnzrg1n8vgdzopps",
     dialect: "mysql",
+    port: 3306,
 }
